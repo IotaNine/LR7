a25eb8de2dfab3c9bbf6795ef54861888e834cbd
IotaNine <70635434+IotaNine@users.noreply.github.com>
2021-03-29 15:37:44 +0300

    Add files via upload

---
+++ Stack.cpp
@@ -0,0 +1,97 @@
+﻿#include <iostream>
+#include <ctime>
+
+struct element
+{
+    element* e_prev = nullptr;
+    int data;
+};
+
+struct Stack
+{
+    element* last;
+    unsigned int size;
+};
+
+void constructor(Stack& S)
+{
+    S.last = nullptr;
+    S.size = 0;
+}
+
+void destructor(Stack& S)
+{
+    while (S.last != nullptr)
+    {
+        element* point = S.last;
+        S.last = S.last->e_prev;
+        delete point;
+        S.size--;
+    }
+}
+
+unsigned int size(const Stack& S)
+{
+    return S.size;
+}
+
+void push(Stack& S, int N)
+{
+    element* E = new element;
+    E->data = N;
+    if (S.last != nullptr)
+    {
+        E->e_prev = S.last;
+    }
+    S.last = E;
+    S.size++;
+}
+
+void pop(Stack& S)
+{
+    if (S.size != 0)
+    {
+        std::cout << "Element out: " << S.last->data << std::endl;
+        element* point = S.last;
+        S.last = S.last->e_prev;
+        delete point;
+        S.size--;
+    }
+    else
+    {
+        std::cout << "The stack is empty" << std::endl;
+    }
+}
+
+void print(const Stack& S)
+{
+    element* point = S.last;
+    while (point != nullptr)
+    {
+        std::cout << point->data << std::endl;
+        point = point->e_prev;
+    }
+    std::cout << std::endl;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    srand(time(NULL));
+    Stack A;
+    constructor(A);
+    int N = 0;
+    std::cout << "How many elements: ";
+    std::cin >> N;
+    for (int i = 0; i < N; i++)
+    {
+        push(A, rand() % 10);
+    }
+    std::cout << "Size of the stack A: " << size(A) << std::endl;
+    print(A);
+    push(A, rand() % 10);
+    print(A);
+    pop(A);
+    print(A);
+    std::cout << "Size of the stack A: " << size(A) << std::endl;
+}
